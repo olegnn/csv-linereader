0ba3b206014ac9c618aed19de30a68c8226d66ca
olegnn <olegnosov1@gmail.com>
2019-10-18 17:22:04 +0300

    Resolve after all handlers are finished

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,6 +2,7 @@ const reader = require('../src');
 
 let performance;
 try {
+  // eslint-disable-next-line
   ({ performance } = require('perf_hooks'));
 } catch (e) {
   performance = {
@@ -16,14 +17,19 @@ const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 describe('reader tests', () => {
   test("it handles file's first line correctly", async () => {
-    let handled = false;
+    let handled = 0;
     const handler = async (data) => {
-      if (!handled) {
+      handled++;
+      if (handled === 1) {
         expect(data).toEqual('1,Stormi,Abrey,sabrey0@timesonline.co.uk,Female,240.183.181.121'.split(','));
+      } else if (handled === 50) {
+        expect(data).toEqual('50,Estelle,Harcarse,eharcarse1d@craigslist.org,Female,73.83.200.1'.split(','));
+      } else if (handled === 100) {
+        expect(data).toEqual('100,Kimberlyn,Abate,kabate2r@purevolume.com,Female,229.132.4.113'.split(','));
       }
-      handled = true;
     };
     await reader('__tests__/test_data.csv', handler, { skipHeader: true });
+    expect(handled).toBe(100);
   });
 
   test('it processes limited operation count at the time', async () => {
@@ -56,6 +62,15 @@ describe('reader tests', () => {
     }
   });
 
+  test('it passes incorrect operation limit', async () => {
+    const handler = async () => {};
+    try {
+      await reader('__tests__/wqwe', handler, { skipHeader: true, operationLimit: 0 });
+    } catch (e) {
+      expect(e).toMatchSnapshot();
+    }
+  });
+
   test('it pauses stream when operationLimit reached', async () => {
     let handleCount = 0;
     const handler = async () => {
@@ -63,6 +78,8 @@ describe('reader tests', () => {
         await sleep(10);
         expect(handleCount).toBe(1);
         setTimeout(() => expect(handleCount).toBe(2), 10);
+      } else if (handleCount === 2) {
+        await sleep(10);
       }
     };
     await reader('__tests__/test_data.csv', handler, { skipHeader: true, operationLimit: 1 });
@@ -73,6 +90,7 @@ describe('reader tests', () => {
     const startTime = performance.now();
     const handler = async () => ++handleCount;
     await reader('__tests__/test_data.csv', handler, { skipHeader: true });
+    // eslint-disable-next-line no-console
     console.log(`100 lines in: ${performance.now() - startTime}`);
     expect(handleCount).toBe(1e2);
   });
--- src/index.js
+++ src/index.js
@@ -4,48 +4,64 @@ const { promisify } = require('util');
 
 const stat = promisify(fs.stat);
 
-module.exports = async (fileName, handler, config = {}) => {
+module.exports = (fileName, handler, config = {}) => new Promise(((async (resolve, reject) => {
   const { delimiter = ',', skipHeader = false } = config;
-  let { operationLimit: availableOperationsCount = 1e4 } = config;
-  let ended = false;
+  const { operationLimit = 1e4 } = config;
+  let reachedEnd = false;
+  let resolved = false;
+  let availableOperationCount = operationLimit;
 
-  if (availableOperationsCount < 1) throw new Error("Operation limit can't be less than 1");
+  if (availableOperationCount < 1) return reject(new Error("Operation limit can't be less than 1"));
 
   let lineReader;
+
   try {
     const fileInfo = await stat(fileName);
     if (!fileInfo.isFile()) {
-      throw new Error(`No such file: ${fileName}`);
+      return reject(new Error(`No such file: ${fileName}`));
     }
     lineReader = readline.createInterface({
       input: fs.createReadStream(fileName),
     });
   } catch (e) {
-    throw new Error(`Failed to create read stream: ${e}`);
+    return reject(new Error(`Failed to create read stream: ${e}`));
   }
 
   const queue = [];
 
-  const onEnd = async () => {
-    while (queue.length) {
-      // eslint-disable-next-line
-      await processHandler(queue.shift());
+  const resolveOnEnd = () => {
+    if (reachedEnd && availableOperationCount === operationLimit && !resolved) {
+      resolve();
+      resolved = true;
     }
-    if (ended) return;
-    if (!availableOperationsCount++) {
-      lineReader.resume();
+  };
+
+  const onHandlerEnd = () => {
+    if (queue.length) {
+      // eslint-disable-next-line no-use-before-define
+      return processHandler(queue.shift());
+    } else {
+      availableOperationCount++;
+      if (reachedEnd) {
+        resolveOnEnd();
+      } else if (availableOperationCount === 1) {
+        lineReader.resume();
+      }
+      return Promise.resolve(null);
     }
   };
 
   const processHandler = line =>
     handler(line.split(delimiter))
-      .then(onEnd)
-      .catch(onEnd);
+      .then(onHandlerEnd)
+      .catch(onHandlerEnd);
 
   const lineHandler = (line) => {
-    if (availableOperationsCount > 0) {
+    if (availableOperationCount > 0) {
+      if (!--availableOperationCount) {
+        lineReader.pause();
+      }
       processHandler(line);
-      if (!--availableOperationsCount) lineReader.pause();
     } else {
       queue.push(line);
     }
@@ -58,9 +74,10 @@ module.exports = async (fileName, handler, config = {}) => {
 
   lineReader.on('line', skipHeader ? firstLineSkipper : lineHandler);
 
-  await new Promise(resolve =>
-    lineReader.on('close', () => {
-      ended = true;
-      resolve();
-    }));
-};
+  lineReader.on('close', () => {
+    reachedEnd = true;
+    resolveOnEnd();
+  });
+
+  return null;
+})));
